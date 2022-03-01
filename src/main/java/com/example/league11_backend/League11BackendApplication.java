9eaa75ba19ffdf582c19cdeb7cd88d8d01f4bcba
suramisam <87141576+suramisam@users.noreply.github.com>
2022-03-01 15:37:00 +0530

    Created spring boot project form spring initializr

---
+++ src/main/java/com/example/league11_backend/League11BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.league11_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class League11BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(League11BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/league11_backend/League11BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.league11_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class League11BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
